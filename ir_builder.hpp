99d65427092feb749797c463209192fc170c4804
giladfrid009 <freidkin.gilad@gmail.com>
2023-01-02 10:36:35 +0200

    .

--- ir_builder.hpp
+++ ir_builder.hpp
@@ -14,7 +14,7 @@ class ir_builder
     template<typename ... Args>
     static std::string format_string(const std::string& format, Args ... args)
     {
-        int size_s = std::snprintf(nullptr, 0, format.c_str(), strignify(args) ...) + 1;
+        int size_s = std::snprintf(nullptr, 0, format.c_str(), as_ctype(args) ...) + 1;
 
         if (size_s <= 0)
         {
@@ -25,7 +25,7 @@ class ir_builder
 
         std::unique_ptr<char[]> buffer(new char[size]);
 
-        std::snprintf(buffer.get(), size, format.c_str(), strignify(args) ...);
+        std::snprintf(buffer.get(), size, format.c_str(), as_ctype(args) ...);
 
         return std::string(buffer.get(), buffer.get() + size - 1);
     }
@@ -39,12 +39,12 @@ class ir_builder
     private:
 
     template<class T>
-    static T const& strignify(T const& arg)
+    static T const& as_ctype(T const& arg)
     {
         return arg;
     }
 
-    static const char* strignify(std::string const& arg)
+    static const char* as_ctype(std::string const& arg)
     {
         return arg.c_str();
     }
