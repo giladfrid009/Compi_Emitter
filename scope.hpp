0a72311da768efe4749e57ceab29afc9db2891d6
giladfrid009 <freidkin.gilad@gmail.com>
2023-01-11 21:50:10 +0200

    .

--- scope.hpp
+++ scope.hpp
@@ -23,23 +23,18 @@ class scope
     const bool loop_scope;
 
     scope(int offset, bool loop_scope);
-
     ~scope();
 
     bool contains_symbol(const std::string& name) const;
-
     bool contains_symbol(const std::string& name, symbol_kind kind) const;
 
     const symbol* get_symbol(const std::string& name) const;
-
     const symbol* get_symbol(const std::string& name, symbol_kind kind) const;
 
     const std::list<const symbol*>& get_symbols() const;
 
     bool add_variable(const std::string& name, type_kind type);
-
     bool add_parameter(const std::string& name, type_kind type);
-
     bool add_function(const std::string& name, type_kind return_type, const std::vector<type_kind>& parameter_types);
 };
 
--- symbol_table.hpp
+++ symbol_table.hpp
@@ -18,26 +18,20 @@ class symbol_table
     static symbol_table& instance();
 
     void open_scope(bool loop_scope = false);
-
     void close_scope();
-
+    
     const scope& current_scope() const;
 
     bool contains_symbol(const std::string& name) const;
-
     bool contains_symbol(const std::string& name, symbol_kind kind) const;
 
     const symbol* get_symbol(const std::string& name) const;
-
     const symbol* get_symbol(const std::string& name, symbol_kind kind) const;
 
     bool add_variable(const std::string& name, type_kind type);
-
     bool add_parameter(const std::string& name, type_kind type);
-
-    bool add_function(const std::string& name, type_kind return_type, const std::vector<type_kind>& parameter_types);
-
     bool add_function(const std::string& name, type_kind return_type);
+    bool add_function(const std::string& name, type_kind return_type, const std::vector<type_kind>& parameter_types);
 
     const std::list<scope>& get_scopes() const;
 };
